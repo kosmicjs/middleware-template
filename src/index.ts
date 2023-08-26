37d3731df4b76c251211ea2649f1488f2bec0e7c
Spencer Snyder <sasnyde2@gmail.com>
2023-08-25 21:10:13 -0400

    chore: fix things up more

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,12 @@
-export type Next = () => Promise<unknown>;
+import {type Context, type Middleware, type Next} from 'koa';
 
-/**
- * Composeable middleware.
- *
- * @param context - The Koa extended context object
- * @param next - The next function to be call the next next middleware in the middleware chain.
- */
-export type Middleware<C = Context> = (
-  /**
-   * The Koa extended context object
-   */
-  context: C,
-  /**
-   * The next function to be call the next next middleware in the middleware chain.
-   * Always returns a promise and allows for fine grained middleware control flow.
-   *
-   */
-  next: Next,
-) => Promise<unknown>;
+type Options = Record<string, unknown>;
 
-/**
- * Expose compositor
- *
- * Compose `middleware` returning
- * a fully valid middleware comprised
- * of all those which are passed.
- */
-
-function compose(middleware: Middleware[]) {
-  if (!Array.isArray(middleware))
-    throw new TypeError('Middleware stack must be an array!');
-  for (const fn of middleware) {
-    if (typeof fn !== 'function')
-      throw new TypeError('Middleware must be composed of functions!');
-  }
-
-  return function (context: Context, next?: Next) {
-    // last called middleware #
-    let index = -1;
-    return dispatch(0);
-    function dispatch(i: number): Promise<unknown> {
-      if (i <= index) Promise.reject(new Error('next() called multiple times'));
-      index = i;
-      let fn: Middleware | undefined = middleware[i];
-      if (i === middleware.length) fn = next;
-      if (!fn) return Promise.resolve();
-      try {
-        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
+function middleware(options?: Options) {
+  // do something here
+  return async function (context: Context, next?: Next) {
+    // do something here
   };
 }
 
-export default compose;
+export default middleware;
---
+++ test/index.ts
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('foo', async (t) => {
+  t.pass();
+});
